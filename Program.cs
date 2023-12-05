1f3628562f4a6bffde7fc97b479137adbdb7d40e
Ethan Chinnasamy <ethan.chinnasamy@standardbank.co.za>
2023-12-05 13:18:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2._21_Static_Attribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Songs holiday = new Songs("Holiday", "Green Day", 4);
+            Songs kashmir = new Songs("Kashmir", "Led Zeppelin", 5);
+
+            Console.Write(Songs.songCount);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Songs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2._21_Static_Attribute
+{
+    internal class Songs
+    {
+
+        public string title;
+        public string artist;
+        public int duration;
+
+        public static int songCount = 0;
+        public Songs(string aTtile, string aArtist, int aDuration)
+        {
+            this.title = aTtile;    
+            this.artist = aArtist;
+            this.duration = aDuration;
+            songCount++;
+        }
+    }
+}
